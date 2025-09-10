458f80f2c5729570c591174d87110595a86459de
CodePierFDEZ <gianpierre.fernandez@tecsup.edu.pe>
2025-09-10 15:11:54 -0500

    feat: Add ViewHolaCurso

---
+++ app/src/main/java/com/example/moviecounter/ViewHolaCurso.kt
@@ -0,0 +1,22 @@
+package com.example.moviecounter
+
+// Pega exactamente esto (código con errores según el enunciado)
+fun ViewHolaCurso() {
+    Column(
+        modifier = Modifier
+            .fillMaxWith()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Welcome to the Course!",
+            fontSize = 28.sp,
+            fontWeight = FontWeigh.Bold
+        )
+        Spacer(modifier = Modifier.heigh(16.dp))
+        Text(
+            text = "Hello, Student!",
+            fontSize = 20.xD
+
+    }
+}
